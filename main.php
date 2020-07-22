b6050bf6f29162df2812c752b44b3dc096c5ec2f
Nicolas Far <nicolasf@nearpod.com>
2020-07-21 20:29:07 -0400

    Add readme

--- main.php
+++ main.php
@@ -3,23 +3,23 @@ require_once __DIR__ . '/vendor/autoload.php';
 use LazyChain\LazyChain;
 
 function foo($i){
-	// echo "evaluating". $i .PHP_EOL;
+	//echo "evaluating". $i .PHP_EOL;
 	return $i;
 }
 
-function all_numbers(){
+function endless_generator(){
 	$i = 0;
 	while(true){
 		yield foo($i++);
 	}
 }
 
-$first_4_even_numbers_squared = (new LazyChain(all_numbers()))
+$all_numbers = endless_generator();
+
+$first_4_even_numbers_squared = (new LazyChain($all_numbers))
 	->filter(fn($n) => $n % 2 == 0 )
 	->map(fn($n) => pow($n,2) )
 	->take(4)
 	->collect();
 
-foreach($first_4_even_numbers_squared as $number){
-	echo $number.PHP_EOL;
-}
+print_r($first_4_even_numbers_squared);
