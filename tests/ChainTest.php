7ca5c63b1978b3e58ccb7c3bfe5663485393a623
Nicolas Far <nicolasf@nearpod.com>
2020-07-23 17:59:36 -0400

    Chain Test

---
+++ tests/ChainTest.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+use LazyChain\LazyChain;
+
+final class ChainTest extends TestCase {
+
+    public function testBasicChain(): void
+	{
+		$iterator_a = new ArrayIterator(['a', 'b', 'c']);
+		$iterator_b = new ArrayIterator(['d', 'e', 'f']);
+		$chainedIterators = (new LazyChain($iterator_a))
+			->chain($iterator_b);
+		
+		$this->assertEquals($chainedIterators->collect(),['a', 'b', 'c','d', 'e', 'f']);
+	}
+	
+	public function testEmpty(): void
+	{
+		$iterator_a = new ArrayIterator([]);
+		$iterator_b = new ArrayIterator(['d', 'e', 'f']);
+		$chainedIterators = (new LazyChain($iterator_a))
+			->chain($iterator_b);
+		
+		$this->assertEquals($chainedIterators->collect(),['d', 'e', 'f']);
+    }
+        
+    public function testbothEmpty(): void {
+		$iterator_a = new ArrayIterator([]);
+		$iterator_b = new ArrayIterator([]);
+		$chainedIterators = (new LazyChain($iterator_a))
+			->chain($iterator_b);
+		
+		$this->assertEquals($chainedIterators->collect(),[]);
+	}
+}
